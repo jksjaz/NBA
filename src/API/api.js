9be064be47477e6080d8a4685b03ffc865f8ed4b
Jaskaran Singh <bestfxon@gmail.com>
2019-05-15 18:53:30 -0500

    Step 2 Complete Need more changes later

--- src/API/api.js
+++ src/API/api.js
@@ -1,11 +1,22 @@
 import store from "../Redux/Store/store"
 import { players, teams } from "../Redux/Actions/actions"
 
-const playerURL = "http://localhost:3008/players"
-const teamURL = "http://localhost:3008/teams"
+const staticURL = "http://localhost:3008/"
+
+const pagination = "?_page=7"
+const querySyntax = "?q="
+
+export const playerURL = `${staticURL}players`
+export const teamURL = `${staticURL}teams`
 
 export const getPlayers = () => {
-    fetch(playerURL).then(items => items.json()).then(res => store.dispatch(players(res)))
+    fetch(playerURL + pagination).then(items => items.json()).then(res => store.dispatch(players(res)))
+}
+
+export const queryData = query => {
+    if (query.length > 1) {
+        fetch(playerURL + querySyntax + query).then(items => items.json()).then(res => store.dispatch(players(res)))
+    }
 }
 
 export const getTeams = () => {
--- src/components/Main/Search/index.js
+++ src/components/Main/Search/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import styles from "./styles";
 
+import { queryData } from "../../../API/api"
+
 const Search = props => {
+    const handleChange = e => {
+        queryData(e.target.value)
+    }
+
     return (
         <div style={{ ...styles.container, ...props.style }}>
-            <input placeholder="Search..." />
+            <input placeholder="Search..." onChange={handleChange}/>
         </div>
     );
 };
